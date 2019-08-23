d172abe3520a3caa525b09623fb6692837dcdcb9
Ningareddy Modase <ningareddy.modase@gmail.com>
2019-08-23 13:33:03 +0530

    Learning React and Redux from udemy

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,30 @@
+import project1 from './../assets/project1.png'
+import project2 from './../assets/project2.png'
+import project3 from './../assets/project3.png'
+
+
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Example React Application',
+        description:'JS and core web dev concepts',
+        link:'https://github.com/ningareddy/react-bootcamp',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'My API',
+        description:'A REST API built from scratch GET and POST',
+        link:'https://github.com/ningareddy/react-bootcamp',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'Operating System Final Project',
+        description:'University final year project',
+        link:'https://github.com/ningareddy/react-bootcamp',
+        image: project3
+    }
+];
+
+export default PROJECTS;
---
+++ portfolio/src/data/socialProfiles.js
@@ -0,0 +1,34 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILE = [
+    {
+        id: 1,
+        title: 'E-mail',
+        link:'https://github.com/ningareddy/react-bootcamp',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        title: 'Github',
+        link:'https://github.com/ningareddy/react-bootcamp',
+        image: githubIcon
+    },
+    {
+        id: 3,
+        title: 'LinkedIN',
+        link:'https://github.com/ningareddy/react-bootcamp',
+        image: linkedinIcon
+    },
+    {
+        id: 4,
+        title: 'Twitter',
+        link:'https://github.com/ningareddy/react-bootcamp',
+        image: twitterIcon
+    },            
+];
+
+
+export default SOCIAL_PROFILE;
